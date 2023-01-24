c3d709d38a70954277cd2bfd18ee825f65f7f0c2
andim461 <andim461@yandex.ru>
2023-01-24 11:18:52 +0100

    Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,41 @@
-import {LatLngExpression, LeafletMouseEvent} from 'leaflet';
-import React, {useState, useEffect} from 'react';
-import {MapContainer, TileLayer, Marker, Popup, LayerGroup, Polygon, Pane, useMapEvents} from 'react-leaflet';
+import {LeafletMouseEvent} from 'leaflet';
+import React, {useState} from 'react';
+import {MapContainer, TileLayer, Polygon, useMapEvents} from 'react-leaflet';
 import './App.css';
 import CountriesSearchBar from './components/CountriesSearchBar/CountriesSearchBar';
 import getCoordinates from './utils/get-coordinates';
 import data from './data/visas.json';
-import { VisaStatus } from './types/VisaStatus';
+import {VisaStatus} from './types/VisaStatus';
 
+enum CountryColors {
+    CURRENT = 'blue',
+    VISA_FREE = 'green',
+    VISA_ON_ARRIVAL = 'lightgreen',
+    E_VISA = 'orange',
+    VISA_REQUIRED = 'red',
+    COVID_BAN = 'gray',
+    DAYS_WITHOUT_VISA = 'yellow'
+}
 
-const CURRENT_COUNTRY = {color: 'blue'};
-const  VISA_FREE_COUNTRY = {color: 'green'};
-const VISA_ON_ARRIVAL_COUNTRY = {color: 'lightgreen'};
-const E_VISA_COUNTRY = {color: 'orange'};
-const VISA_REQUIRED_COUNTRY = {color: 'red'};
-const COVID_BAN_COUNTRY = {color: 'gray'};
-const DAYS_WITHOUT_VISA = {color: 'yellow'};
-
-
-
-
-
-function CustomMap(props: { setSelected: (country: string) => void; selected: string; }) {
-    const map = useMapEvents({
+function CustomMap(props: {setSelected: (country: string) => void; selected: string}) {
+    useMapEvents({
         click: (e: LeafletMouseEvent) => {
-            
             const lat: number = e.latlng.lat;
             const lng: number = e.latlng.lng;
-            fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}&zoom=18&addressdetails=1&accept-language=en`)
-           .then(data => data.json())
-           .then(dataJSON => dataJSON.error
-                            ? console.log(dataJSON) 
-                            : props.setSelected(dataJSON.address.country));
-            
+            fetch(
+                `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}&zoom=18&addressdetails=1&accept-language=en`
+            )
+                .then((data) => data.json())
+                .then((dataJSON) =>
+                    dataJSON.error ? console.log(dataJSON) : props.setSelected(dataJSON.address.country)
+                );
         }
     });
     const coords = getCoordinates(props.selected);
-    if(coords === null) {
+    if (coords === null) {
         return null;
     }
-    else {
-        return <Polygon pathOptions={CURRENT_COUNTRY} positions={coords} /> ;
-    }
+    return <Polygon pathOptions={{color: CountryColors.CURRENT}} positions={coords} />;
 }
 
 function paintAllCountries(passport: string) {
@@ -48,63 +43,58 @@ function paintAllCountries(passport: string) {
 
     const visasData = data as VisaStatus[];
     const currentVisaStatus = visasData.find((visa) => visa.Passport === passport);
-    if (passport !== ''){
+
+    if (passport !== '') {
         for (let destinationCountry in currentVisaStatus) {
-            if(destinationCountry !== passport) {
-                const status = currentVisaStatus[destinationCountry];
-                const coords = getCoordinates(destinationCountry);
-                let pathOptions;
-                switch(status) {
-                    case 'covid ban':
-                        pathOptions = COVID_BAN_COUNTRY;
-                        break;
-                    case 'e-visa':
-                        pathOptions = E_VISA_COUNTRY;
-                        break;
-                    case 'visa free':
-                        pathOptions = VISA_FREE_COUNTRY;
-                        break;
-                    case 'visa on arrival':
-                        pathOptions = VISA_ON_ARRIVAL_COUNTRY;
-                        break;
-                    case 'visa required':
-                        pathOptions = VISA_REQUIRED_COUNTRY;
-                        break;
-                    default:
-                        pathOptions = DAYS_WITHOUT_VISA;
-                        break;        
-                }
-                if(coords) {
-                    polygons.push(
-                        <Polygon pathOptions={pathOptions} positions={coords} />
-                    )
-                }
+            if (destinationCountry === passport) {
+                continue;
+            }
+
+            const status = currentVisaStatus[destinationCountry];
+            const coords = getCoordinates(destinationCountry);
+            let pathOptions;
+            switch (status) {
+                case 'covid ban':
+                    pathOptions = CountryColors.COVID_BAN;
+                    break;
+                case 'e-visa':
+                    pathOptions = CountryColors.E_VISA;
+                    break;
+                case 'visa free':
+                    pathOptions = CountryColors.VISA_FREE;
+                    break;
+                case 'visa on arrival':
+                    pathOptions = CountryColors.VISA_ON_ARRIVAL;
+                    break;
+                case 'visa required':
+                    pathOptions = CountryColors.VISA_REQUIRED;
+                    break;
+                default:
+                    pathOptions = CountryColors.DAYS_WITHOUT_VISA;
+                    break;
+            }
+            if (coords) {
+                polygons.push(
+                    <Polygon pathOptions={{color: pathOptions}} positions={coords} key={destinationCountry} />
+                );
             }
         }
     }
-    
 
     return polygons;
 }
 
-
-
-
 function App() {
-    
     const [countryName, setCountryName] = useState('');
-
 
     return (
         <div className="App">
-            <CountriesSearchBar selected={countryName} setSelected={setCountryName}/>
-            <MapContainer className="map" center={[51.505, -0.09]} zoom={2} scrollWheelZoom={true}>
-                
+            <CountriesSearchBar selected={countryName} setSelected={setCountryName} />
+            <MapContainer className="map" center={[51.505, -0.09]} zoom={2} scrollWheelZoom={true} minZoom={2}>
                 <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                <CustomMap selected={countryName} setSelected={setCountryName}/>
+                <CustomMap selected={countryName} setSelected={setCountryName} />
                 {paintAllCountries(countryName)}
             </MapContainer>
-            
         </div>
     );
 }
